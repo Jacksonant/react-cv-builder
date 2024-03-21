31acb7af4e4d19a8ef118a43d64abfcdbe77564f
Jacksonant <limjunhao@live.com.my>
2024-03-21 23:39:00 +0800

    edit img base url

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: "export",
+  basePath: isProd ? '/react-cv-builder' : '',
+  // distDir : 'dist', // if want to change the output directory name
 };
 
 module.exports = nextConfig;
